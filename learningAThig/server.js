d762ef76d8f72a3f96fbc93f0d16e928be931203
SassanKermani <SassanKermani2@gmail.com>
2018-02-21 10:06:44 -0700

    day one

--- learningAThig/server.js
+++ learningAThig/server.js
@@ -7,6 +7,10 @@
 let express = require('express');
 let app = express();
 
+//body-parser
+let bodyParser = require('body-parser');
+app.use(bodyParser());
+
 //pg 
 let pg = require('pg');
 const connectionString = 'postgres://localhost:5432/theinfo';		//conection string
@@ -16,7 +20,7 @@ client.connect();													//conecting
 /*=====  End of Bringing in npm's and seting them up  ======*/
 
 /*----------  test to make sure it works  ----------/
-client.query("SELECT * FROM pg_catalog.pg_tables;")
+client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
 	.then(function(data){
 		console.log(data);
 	})
@@ -33,6 +37,37 @@ client.query("SELECT * FROM pg_catalog.pg_tables;")
 app.get('/', function(req, res){	
 	res.send(" hey welcom to my api it's going to do some stuff at some point");
 });
+
+app.get('getAllTables', function(req, res){
+	client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+		.then(function(data){
+			console.log(data);
+			res.send(data);
+		})
+		.catch(function(error){
+			console.log(error);
+			res.send(error);
+		});
+})
+
+//sending a sql and responding with responce form the db
+app.post('/plainSql', function(req, res){
+	//for testing purpeses
+	// console.log("hit plainSql rout")
+	// console.log(req.body.sql);
+	// res.send(req.body.sql);
+	client.query(req.body.sql)
+	.then(function(data){
+		console.log(data);
+		res.send(data);
+	})
+	.catch(function(error){
+		console.log(error);
+		res.send(error);
+	});
+});
+
+
 
 /*=====  End of routs  ======*/
 

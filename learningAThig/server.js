f0b547ea81a11c0b2eee363fb8f19186c82edffd
SassanKermani <SassanKermani2@gmail.com>
2018-02-21 10:47:36 -0700

    wow i like did a lot of shit so far

--- learningAThig/server.js
+++ learningAThig/server.js
@@ -13,7 +13,11 @@ app.use(bodyParser());
 
 //pg 
 let pg = require('pg');
-const connectionString = 'postgres://localhost:5432/theinfo';		//conection string
+//const connectionString = 'postgres://localhost:5432/theinfo';		//conection string
+
+//for testing with a db with mose data in it
+const connectionString = 'postgres://localhost:5432/carmen';		//conection string
+
 const client = new pg.Client(connectionString);						//seting up the connection
 client.connect();													//conecting
 
@@ -38,17 +42,23 @@ app.get('/', function(req, res){
 	res.send(" hey welcom to my api it's going to do some stuff at some point");
 });
 
-app.get('getAllTables', function(req, res){
+app.get('/seeAllTables', function(req, res){	
+	//for testing
+	// res.send(" you hit the seeAllTables rout ");
+	// console.log("you hit the seeAllTables rout");
+
 	client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
 		.then(function(data){
-			console.log(data);
-			res.send(data);
+			//console.log(data);
+			//res.send(data);
+			res.send(data.rows);
 		})
 		.catch(function(error){
-			console.log(error);
+			//console.log(error);
 			res.send(error);
 		});
-})
+
+});
 
 //sending a sql and responding with responce form the db
 app.post('/plainSql', function(req, res){
@@ -58,16 +68,32 @@ app.post('/plainSql', function(req, res){
 	// res.send(req.body.sql);
 	client.query(req.body.sql)
 	.then(function(data){
-		console.log(data);
+		//console.log(data);
 		res.send(data);
 	})
 	.catch(function(error){
-		console.log(error);
+		//console.log(error);
 		res.send(error);
 	});
 });
 
+/*----------  this is gana be a thing buckle up buter cup time for a some looping bulshit  ----------*/
 
+client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+	.then(function(data){
+		//console.log(data);
+
+		//bulshit start hear
+		let i = 0;
+		while(data.rows[i] != null){
+			console.log(i)
+			console.log(data.rows[i].table_name);
+			i = i + 1;
+		}
+	})
+	.catch(function(error){
+		//console.log(error);
+	});
 
 /*=====  End of routs  ======*/
 
